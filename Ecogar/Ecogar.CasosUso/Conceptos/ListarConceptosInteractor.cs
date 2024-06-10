9b9174a0c541d0ebdf23dc273e400d0d73e8fde6
FranSparda <javierfran2409@gmail.com>
2024-06-10 19:08:43 +0200

    Finish page 3
    Tiene un error que aun no he conseguido solucionar y unas cuantas advertencias

---
+++ Ecogar/Ecogar.CasosUso/Conceptos/ListarConceptosInteractor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecogar.DTOs.Conceptos;
+using Ecogar.Entidades.Interfaces.Conceptos;
+using Ecogar.Puertos.Conceptos;
+
+namespace Ecogar.CasosUso.Conceptos
+{
+    public class ListarConceptosInteractor : ListarConceptosInputPort
+    {
+        private readonly ListarConceptosOutputPort _outputPort;
+        private readonly IListarConceptos _listarConceptos;
+
+        public ListarConceptosInteractor(
+            ListarConceptosOutputPort outputPort, 
+            IListarConceptos listarConceptos)
+        {
+            _outputPort = outputPort;
+            _listarConceptos = listarConceptos;
+        }
+
+        public Task Ejecutar()
+        {
+            var conceptos = _listarConceptos.Listar();
+            var salida = conceptos.Select(c => new ConceptoDto(c)).ToList();
+            _outputPort.Ejecutar(salida);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Ecogar/Ecogar.CasosUso/ContenerdorDependencias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecogar.CasosUso.Conceptos;
+using Ecogar.Puertos.Conceptos;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecogar.CasosUso
+{
+    public static class ContenerdorDependencias
+    {
+        public static IServiceCollection Interactores(this IServiceCollection services)
+        {
+            services.AddTransient<CrearConceptoInputPort, CrearConceptoInteractor>();
+            services.AddTransient<ModificarConceptoInputPort, ModificarConceptoInteractor>();
+            services.AddTransient<BorrarConceptoInputPort, BorrarConceptoInteractor>();
+            services.AddTransient<ListarConceptosInputPort, ListarConceptosInteractor>();
+
+            return services;
+        }
+    }
+}
